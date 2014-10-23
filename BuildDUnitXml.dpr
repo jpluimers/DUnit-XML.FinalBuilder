1f2062ffcd904b3990c299381137279042e1d01d
Jeroen Wiert Pluimers <jeroenp@bitbucket.org>
2014-10-23 06:23:26 +0200

    Simple BuildDUnitXml console app to ensure FinalBuilder.XMLTestRunner builds in various Delphi versions.

---
+++ BuildDUnitXml.dpr
@@ -0,0 +1,11 @@
+program BuildDUnitXml;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  FinalBuilder.XMLTestRunner in 'FinalBuilder.XMLTestRunner.pas';
+
+begin
+  // simple console app just to ensure FinalBuilder.XMLTestRunner builds in various Delphi versions.
+end.
